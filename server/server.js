cc5e1910817ce0d9bac6569110a79236677c19db
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-31 13:12:58 -0800

    incorporated graphql more into server

--- server/server.js
+++ server/server.js
@@ -33,4 +33,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
