53142f50e6dc8b52755e177a8b41361b77f7f370
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-31 13:08:25 -0800

    installed apollo server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 
 // Imports
 const db = require('./config/connection');
